b711329d3e657d192e85d7a2a52aa613fdbce25e
lordonez <mar.escobar1991@gmail.com>
2022-11-08 11:26:13 -0600

    boton de crear cuenta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" >
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6" style="background-color: #e3e9f1; border-radius: 25px;">
             <div class="">
@@ -13,56 +13,63 @@
                         @csrf
 
                         <div class="form-group row">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Correo Electronico *" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Correo Electronico *" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+
                         </div>
 
                         <div class="form-group row">
 
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Contraseña *" required autocomplete="current-password">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Contraseña *" required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                          
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+
                         </div>
 
                         <div class="form-group row">
-                           
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                         
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+
                         </div>
 
                         <div class="form-group row mb-0">
-                            
-                    
-                                <button type="submit" class="btn btn-hover-fill"><i
-                                        class="icon-right-arrow"></i><span>Entrar</span><i
-                                        class="icon-right-arrow"></i></button>
-                        
 
-                                @if (Route::has('password.request'))
-                                    <a class="" href="{{ route('password.request') }}" style="padding-top: 3%; padding-left: 5%;">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            
+
+                            <button type="submit" class="btn btn-hover-fill"><i class="icon-right-arrow"></i><span>Entrar</span><i class="icon-right-arrow"></i></button>
+
+
+                            @if (Route::has('password.request'))
+                            <a class="" href="{{ route('password.request') }}" style="padding-top: 3%; padding-left: 5%;">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                            @endif
+
                         </div>
                     </form>
+                    <br>
+                    <div class="h-decor"></div>
+                    <div class="form-group row" style="padding-left: 35%;" >
+
+                     <center><a class="btn btn-hover-fill" href="{{ url('seguridad/user/create') }}" style="background-color: #4fb366; border-color: #4fb366;"><i class="icon-right-arrow"></i><span>Crear Cuenta</span><i class="icon-right-arrow"></i></a></center>   
+
+
+
+                    </div>
                 </div>
             </div>
         </div>
